baaef0b7d91d9ab8f26aa17b29691facf68f28ea
it22582492 <127284582+it22582492@users.noreply.github.com>
2024-12-08 19:43:19 +0530

    day2

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,9 +9,51 @@ namespace E_Shopping_Platform
 {
     public partial class Default : System.Web.UI.Page
     {
+        public static String CS = "Data Source=SETHMINA\\SQLEXPRESS;Initial Catalog=MyShoppingDB;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["UserLogin"] == "YES")
+            {
+                Response.Redirect("UserHome.aspx?UserLogin=YES");
+            }
 
+            if (Session["Username"] != null)
+            {
+                //lblSuccess.Text = "Login Success, Welcome <b>" + Session["Username"].ToString() + "</b>";
+
+                if (!this.IsPostBack)
+                {
+                    BindProductRepeater();
+                    btnSignUP.Visible = false;
+                    btnSignIN.Visible = false;
+                    btnlogout.Visible = true;
+                }
+
+            }
+            else
+            {
+                BindProductRepeater();
+                btnSignUP.Visible = true;
+                btnSignIN.Visible = true;
+                btnlogout.Visible = false;
+                //Response.Redirect("Default.aspx");
+                Response.Write("<script type='text/javascript'>alert('Login plz')</script>");
+
+            }
+        }
+        public void BindCartNumber()
+        {
+            if (Request.Cookies["CartPID"] != null)
+            {
+                string CookiePID = Request.Cookies["CartPID"].Value.Split('=')[1];
+                string[] ProductArray = CookiePID.Split(',');
+                int ProductCount = ProductArray.Length;
+                pCount.InnerText = ProductCount.ToString();
+            }
+            else
+            {
+                pCount.InnerText = 0.ToString();
+            }
         }
     }
 }
